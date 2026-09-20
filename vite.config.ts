8dc0d830137d7b7773eb368ca867125acb8e7036
salaihhasan <salaihhasan@gmail.com>
2026-09-20 17:33:15 +0530

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -22,7 +22,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
 
-  base: '/dristi-portfolio/',
+   base: '/dristi-portfolio/',
 
   server: {
     port: 3000,

6227a43c0f12b26d833b8768d94bbb95e3f2879f
salaihhasan <salaihhasan@gmail.com>
2026-09-20 17:10:39 +0530

    Update portfolio

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,34 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 3000,
+//     open: false,
+//   },
+//   build: {
+//     target: 'es2020',
+//     sourcemap: true,
+//   },
+// });
+
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  base: '/dristi-portfolio/',
+
   server: {
     port: 3000,
     open: false,
   },
+
   build: {
     target: 'es2020',
     sourcemap: true,
